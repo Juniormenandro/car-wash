1e399a9ca427c6f9c5955c063d6e53a4ea91f14f
juniorwa <100045666+juniorwa@users.noreply.github.com>
2023-08-07 19:38:11 +0100

    hi

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -10,14 +10,14 @@ const Header: React.FC = () => {
   return (
     <header className="flex "
     style={{
-      
+      maxHeight:"200px",
       paddingTop:"13%",
       paddingBottom:"10%",
-     
+     marginTop:"5px",
       backgroundImage: `url('/images/logo.jpg')`,
       backgroundPosition: 'center', // Centraliza a imagem de fundo
       backgroundRepeat: 'no-repeat', // Previne a repetição da imagem
-      backgroundSize: '480px' // Faz a imagem cobrir toda a área disponível
+      backgroundSize: '330px' // Faz a imagem cobrir toda a área disponível
     }}>
    
   </header>
